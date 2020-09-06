4bc77670f23e20ad95a567693f3b03e056c4c0b0
Junak <junak.pub@gmail.com>
2020-09-06 23:59:59 +0300

    drm: panel: mipi-dsi-generic: new driver

---
+++ include/linux/platform_data/dsi_backlight.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0-only
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved. */
+
+#ifndef _INCLUDE_LINUX_PLATFORM_DATA_DSI_BACKLIGHT_H
+#define _INCLUDE_LINUX_PLATFORM_DATA_DSI_BACKLIGHT_H
+
+#include <linux/spinlock.h>
+
+struct mipi_dsi_device;
+struct backlight_device;
+struct dsi_backlight_platform_data;
+
+typedef int (*mipi_dsi_backlight_init_cb)(struct dsi_backlight_platform_data *data);
+
+struct dsi_backlight_platform_data {
+	struct mipi_dsi_device *dsi;
+	struct backlight_device *backlight;
+	mipi_dsi_backlight_init_cb backlight_init;
+	bool prepared;
+	void *userdata;
+};
+
+#endif /* _INCLUDE_LINUX_PLATFORM_DATA_DSI_BACKLIGHT_H */
